377dffd87c13cb2ac4f6368e827c52f098d7a129
kpmorina <kpmorina95@gmail.com>
2024-09-29 19:45:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+FizzBuzz
+
 for ( let a = 1; a <= 100; a++ ){
     if (a %3 === 0 && a %5 === 0 ) {
         console.log("Fizz Buzz")
@@ -9,3 +11,24 @@ for ( let a = 1; a <= 100; a++ ){
         console.log(a)
     }
 } 
+
+Prime Time
+
+let n = 0;
+while (n <= 20) {
+    if (n < 2) {
+        n++;
+        continue;
+    }
+    let isPrime = true;
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log(n);
+    }
+    n++;
+}
